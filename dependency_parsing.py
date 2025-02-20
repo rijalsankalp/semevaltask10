47f53159d0d7490dd97caf53ae3976e07346fffb
Sankalpa Rijal <srijal@villanova.edu>
2025-02-20 14:08:24 -0500

    Attempt running semantic similarity for exact match ratio

---
+++ dependency_parsing.py
@@ -0,0 +1,169 @@
+####  Need to install dependencies for coreferee
+### en_core_web_lg is needed even if using en_core_web_trf
+
+# pip install spacy
+# python -m spacy download en_core_web_lg
+# python -m spacy download en_core_web_trf
+# pip install coreferee
+# python -m coreferee install en
+
+
+import spacy
+from spacy import displacy
+import numpy as np
+from LoadData import LoadData
+from pathlib import Path
+from itertools import chain
+
+np.random.seed(0)
+
+
+def get_role_labels():
+    """
+    Returns main roles and fine-grained roles.
+    """
+    # all_main_roles = ["Protagonist", "Antagonist", "Innocent"]
+    # all_sub_roles = ["Guardian", "Martyr", "Peacemaker", "Rebel", "Underdog", "Virtuous", "Instigator", "Conspirator", "Tyrant", "Foreign Adversary", "Traitor", "Spy", "Saboteur", "Corrupt", "Incompetent", "Terrorist", "Deceiver", "Bigot", "Forgotten", "Exploited", "Victim", "Scapegoat"]
+    sub_dict = {
+        "Protagonist":["Guardian", "Martyr", "Peacemaker", "Rebel", "Underdog", "Virtuous"],
+        "Innocent":["Forgotten", "Exploited", "Victim", "Scapegoat"],
+        "Antagonist":["Instigator", "Conspirator", "Tyrant", "Foreign Adversary", "Traitor", "Spy", "Saboteur", "Corrupt", "Incompetent", "Terrorist", "Deceiver", "Bigot"]
+    }
+
+    fg_roles = list()
+    sub_dict['main_role_list'] = list()
+    main_roles_order = ['Protagonist', 'Innocent', 'Antagonist']
+    for main_role in main_roles_order:
+        fg_roles.extend(sub_dict[main_role])
+        sub_dict['main_role_list'].extend([main_role]*len(sub_dict[main_role]))  # creates a list of (repeated) main role labels, redundant, but works for simplicty
+     
+    sub_dict['role_list'] = fg_roles  # list of fine-grained roles
+    fg_to_main = dict()
+    # create a dict mapping fine-grained to main role.
+    for main_role in sub_dict:
+        for fg_role in sub_dict[main_role]:
+            fg_to_main[fg_role] = main_role
+
+    return sub_dict
+
+def load_text_file(path):
+    """
+    Load document in `path` and returns a raw string.
+    """
+
+    with open(path) as fin:
+        text = fin.read()
+    return text
+
+def load_document(base_dir, lang, article_id):
+    """
+    Load a raw document for given path and `article_id`.
+    """
+
+    path = Path(base_dir).joinpath(lang, "raw-documents", article_id)
+    text = load_text_file(path)
+    return text
+
+
+def visualize_dependency_parsing(sents, port=5005):
+    """
+    Visualize the dependency parsing of input sentences.
+    More info: https://spacy.io/usage/visualizers/
+
+    """
+    displacy.serve(sents, style='dep', port=port)
+
+def find_entity_sentence(doc, start_offset, end_offset):
+    """
+    Returns the index of the sentence containing the entity between given indices.
+    """
+
+    for i, sent in enumerate(doc.sents):
+        if sent[0].idx <= start_offset and sent[-1].idx >= end_offset:
+            return i
+
+
+def get_entity_coref_sentences(doc, doc_labels):
+    """
+    Returns the sentences containing the entity and co-references.
+    """
+    # Find the actual entity span form the doc
+    #print(f"doc text = {doc.text}")
+    start_offset = doc_labels['start_offset']
+    end_offset = doc_labels['end_offset']
+
+    # Adjust start_offset to the left if it's in the middle of a word
+    while start_offset > 0 and not doc.text[start_offset - 1] in {' ', '\n', ',', '-', '.'}:
+        start_offset -= 1
+
+    # Adjust end_offset to the right if it's in the middle of a word
+    while end_offset < len(doc.text) and not doc.text[end_offset] in {' ', '\n', ',', '-', '.'}:
+        end_offset += 1
+
+
+    # Get the entity span from start to end offsets
+    entity_span = doc.char_span(start_offset, end_offset, 
+                                label="target_ent",
+                                alignment_mode='expand')   # using 'expand' alignment because some labeled indices do not match spacy's char index
+    
+
+    # Make dictionary of co-reference chains
+    entity_corefs = list()
+    for g in doc._.coref_chains.chains:
+        #print(f"{g.index=}, {g.mentions}, {g.most_specific_mention_index=}")
+        for mention_id in g.mentions:
+            # if entity_span.root.i in mention_id:
+            if any(token.i in mention_id for token in entity_span):
+                entity_corefs.extend(g.mentions)
+                continue
+    # Unpack coref list
+    entity_corefs = list(chain.from_iterable(entity_corefs))
+    coref_sents = list({doc[tid].sent for tid in entity_corefs})  # Use a set to avoid repeated sentences
+
+    
+    return entity_span, entity_corefs, coref_sents
+
+
+def iterate_documents(base_dir, labels, subdirs):
+    """
+    Returns a generator that reads all documents given in input labels_file.
+    Yields a tuple (doc, ent_span, ent_corefs, ent_sents).
+    doc is the SpaCy document.
+    ent_span is the span containing the target entity.
+    ent_corefs are co-references to the entity.
+    ent_sents are the sentences with co-references to the target.
+    """
+
+    nlp = spacy.load("en_core_web_trf")
+    nlp.add_pipe('coreferee')
+
+    for row_id in range(len(labels)):
+        doc_labels = labels.iloc[row_id]
+        text = load_document(base_dir, 'EN', doc_labels['article_id'])
+
+        doc = nlp(text)
+        # Retrieve entity info and coreference
+        ent_span, ent_corefs, ent_sents = get_entity_coref_sentences(doc, doc_labels)
+        
+        yield doc, ent_span, ent_corefs, ent_sents, doc_labels
+
+
+if __name__ == "__main__":
+    base_dir = "train"
+    labels_file = "subtask-1-annotations.txt"
+    subdirs = ["EN"]
+
+    ld = LoadData()
+    labels = ld.load_data(base_dir, labels_file, subdirs)
+    # Cast these columns as int
+    labels['start_offset'] = labels['start_offset'].astype(int)
+    labels['end_offset'] = labels['end_offset'].astype(int)
+
+    for doc, ent_span, ent_corefs, ent_sents, doc_labels in iterate_documents(base_dir, labels, subdirs):
+        print(ent_span)
+        for tid in ent_corefs:
+            tk = doc[tid]
+            print(f"{tk} | {tk.head=} | {list(t for t in tk.children)}")
+
+        visualize_dependency_parsing(ent_sents)
+
---
+++ semantic_similarity.py
@@ -0,0 +1,179 @@
+
+import numpy as np
+from pathlib import Path
+from spacy.lang.en.stop_words import STOP_WORDS
+import re
+
+from dependency_parsing import iterate_documents, get_role_labels
+
+from gensim.models import KeyedVectors
+
+from LoadData import LoadData
+
+
+def load_glove_vectors(path):
+    """
+    Load GloVe embedding vectors in `path`.
+    """
+    model = KeyedVectors.load_word2vec_format(path, binary=False, no_header=True)
+
+    return model
+
+
+def get_role_vectors(model, role_words, n_grams='last'):
+    """
+    For each role word, extract the respective word vector.
+    If a role word is an n-gram - return the average vector of the non-stopwords.
+    """
+
+    x = list()
+    
+    for r in role_words:
+        tokens = re.split("\s+", r.lower())
+        tokens = [t for t in tokens if t not in STOP_WORDS and t in model]
+        if n_grams == 'average':
+            v = np.mean([model[t] for t in tokens], axis=0)  # take the average vector of n-grams
+        elif n_grams == 'last':
+            v = np.array(model[tokens[-1]])  # use the last token in the n-gram
+        x.append(v)
+    
+    x = np.row_stack(x)  # Creates an n x d matrix of `n` fine-grained roles and `d`-dimensional vectors
+    print(x.shape)
+    return x
+    
+
+def get_semantic_sim(word, model, role_vectors):
+    """
+    Returns a vector of cosine similarities between input `word` and role_words using `model`.
+
+    Args:
+        role_vectors (2D-array) : 2D matrix of word vectors (n x d)
+    """
+
+    # cosine similarity: np.dot(u,v)/(np.linalg.norm(u)*np.linalg.norm(v))
+    x = np.dot(role_vectors, model[word])/(np.linalg.norm(model[word])*np.linalg.norm(role_vectors, axis=1))
+    # Apply softmax to turn the similarities into a probability distribution
+    # x = np.exp(x)/np.sum(np.exp(x))
+    x = np.exp(x)
+
+    return x
+
+
+def process_sentence(sent, emb_model, role_vectors,
+                     ent_span,
+                     doc, 
+                     c_window=None,
+                     reduce='max'):
+    """
+    Process sentence and the words therein.
+
+    Args:
+        sent (spacy.sentence): The parsed sentences.
+        emb_model (gensim.word2vecModel): Word embedding.
+        role_vectors (np.array): Numpy word vectors for the roles.
+        ent_span (spacy.Span): The span containing the entity.
+        c_window (int): The size of the context window around the entity.
+        reduce (str): Reduction method {'max', 'average', ...}
+    """
+
+    x_stack = list()
+    x_var = list()
+
+    if c_window is None:
+        context_words = sent
+    else:
+        ent_i = ent_span.root.i
+        context_words = [t for t in doc[max(0, ent_i-c_window):ent_i]] + [t for t in doc[ent_i+1:ent_i+1+c_window]]  # get both sides of the entity span
+    #print(f"{context_words=}")
+    for token in context_words:
+        if token.pos_ in {'NOUN', 'VERB', 'ADJ'}:
+            if token.text.lower() in emb_model:
+                x = get_semantic_sim(token.text.lower(), emb_model, role_vectors)
+                x_stack.append(x)
+                x_var.append(np.var(x))  # save the in-vector variance to use as weight. Vectors with high variance means some of the classes potentially dominate.
+    
+    #instance when there no word in the context window
+    if not x_stack:
+        return np.zeros(role_vectors.shape[0])
+    x_stack = np.row_stack(x_stack)
+
+    if reduce == 'max':
+        x = np.max(x_stack, axis=0)
+    elif reduce == 'average':
+        x = np.mean(x_stack, axis=0)
+    elif reduce == 'w_average':
+        x = np.average(x_stack, axis=0, weights=x_var)
+
+    return x
+    
+
+if __name__ == "__main__":
+    base_dir = "train"
+    labels_file = "subtask-1-annotations.txt"
+    subdirs = ["EN"]
+
+    ld = LoadData()
+    labels = ld.load_data(base_dir, labels_file, subdirs)
+    # Cast these columns as int
+    labels['start_offset'] = labels['start_offset'].astype(int)
+    labels['end_offset'] = labels['end_offset'].astype(int)
+
+    role_labels = get_role_labels()
+    emb_model = load_glove_vectors("embeddings/glove.6B.100d.txt")
+
+    role_vectors = get_role_vectors(emb_model, role_labels['role_list'])
+    c_window = None  # size of context window for model
+
+    max_match_count = 0
+    avg_match_count = 0
+    #w_avg_mathch_count = 0
+    total_instances = 0
+    empty_instances = 0
+
+    for doc, ent_span, ent_corefs, ent_sents, doc_labels in iterate_documents(base_dir, labels, subdirs):
+        print("=====", ent_span)
+
+        # TODO: Navigate the dependency tree to identify the relevant words
+        # E.g.: Secretary of State Medeleine Albright .... declared ... death ... was worth it
+        # Some dependencies to consider:
+        # relcl (relative clause) (top-down, children) : Albright declared that...
+        # ccomp from relcl child (declare ... <was>)
+        # nsubj head from ccomp child (<death> .. > was)
+        x_sents = list()
+        for sent in ent_sents:
+            
+            x = process_sentence(sent, emb_model, role_vectors, ent_span, doc, c_window)
+            # print(x)
+            # print(f"Highest score {np.max(x)} | fg-role: {role_labels['role_list'][np.argmax(x)]} | main role: {role_labels['main_role_list'][np.argmax(x)]}")
+            #print(f"True labels: {doc_labels['sub_roles']=} | {doc_labels['main_role']=}")
+            x_sents.append([x])
+            
+        if len(x_sents) <= 4:
+            total_instances += 1
+            empty_instances += 1
+            continue
+        #x_sents = np.row_stack(x_sents)
+        x_sents = np.array(x_sents)
+        #print(x_sents.shape)
+
+        x_max = np.max(x_sents, axis=0)
+        x_avg = np.mean(x_sents, axis=0)
+        #x_w_avg = np.average(x_sents, axis=0, weights=np.var(x_sents, axis=0))
+
+        max_role = role_labels['role_list'][np.argmax(x_max)]
+        avg_role = role_labels['role_list'][np.argmax(x_avg)]
+        #w_avg_role = role_labels['role_list'][np.argmax(x_w_avg)]
+
+        #print(f"Max role: {max_role}")
+        #print(f"Average role: {avg_role}")
+        # print(f"Weighted average role: {w_avg_role}")
+
+        true_roles = set(doc_labels['sub_roles'])
+        
+        max_match_count += 1 if [max_role] == true_roles else 0
+        avg_match_count += 1 if [avg_role] == true_roles else 0
+        #w_avg_mathch_count += 1 if w_avg_role == true_roles else 0
+        total_instances += 1
+
+    print(f"Total instances: {total_instances}, Empty instances: {empty_instances}")
+    print(f"Exact match ratio - Max: {max_match_count / total_instances}, Average: {avg_match_count / total_instances}") 
